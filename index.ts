26c5bf0d1be8ebcac0d97c26a99ef0d2f662e580
MtJakhar <thvsd.10fingers@gmail.com>
2023-04-04 13:07:55 -0400

    added tables to prisma

---
+++ index.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+// create
+// async function main() {
+//   await prisma.user.create({
+//     data: {
+//       name: 'Alice',
+//       email: 'alice@prisma.io',
+//       posts: {
+//         create: { title: 'Hello World' },
+//       },
+//       profile: {
+//         create: { bio: 'I like turtles' },
+//       },
+//     },
+//   })
+
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   })
+//   console.dir(allUsers, { depth: null })
+// }
+
+// update
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  })
+  console.log(post)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
